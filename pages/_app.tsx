7cbbdd16e6fac6938dcbfa6f45f02447d2b61e1c
NelsonDong41 <nelsondong41@gmail.com>
2023-07-16 00:20:12 -0400

    file setup

---
+++ pages/_app.tsx
@@ -0,0 +1,12 @@
+
+import { CssBaseline } from "@mui/material";
+import type { AppProps } from "next/app";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </>
+  );
+}
