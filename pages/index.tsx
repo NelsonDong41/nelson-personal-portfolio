0e21ed89583984c62a86de8e402a0961ed92d402
NelsonDong41 <nelsondong41@gmail.com>
2023-07-16 00:41:35 -0400

    Layout component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,9 @@
+import Layout from "@/components/Layout"
+
 export default function HomePage() {
-    return <div>hello</div>
+    return <>
+        <Layout>
+            <div>hello</div>
+        </Layout>
+    </>
 }
