19c32a040ac083c67522b4c9398e9714f56df61d
NelsonDong41 <nelsondong41@gmail.com>
2023-07-18 17:07:10 -0400

    save theme mode in local storage

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,22 +7,29 @@ import {
 } from "@mui/material";
 import type { AppProps } from "next/app";
 import "@/styles/globals.css";
-import { useMemo, useState, createContext } from "react";
+import { useMemo, useState, createContext, useEffect } from "react";
 import { getDesignTokens } from "@/styles/theme";
 import { ColorModeContent } from "@/types";
-
 
 export const ColorModeContext = createContext<ColorModeContent>({
   toggleColorMode: () => {}
 })
 
 export default function App({ Component, pageProps }: AppProps) {
-  const [mode, setMode] = useState<PaletteMode>("light");
+  const [mode, setMode] = useState<PaletteMode>("dark");
+
+  useEffect(() => {
+    setMode(localStorage.getItem('mode') === "dark" ? "dark" : "light")
+  }, [])
+
   const colorMode = useMemo(
     () => ({
       toggleColorMode: () => {
-        setMode((prevColor: PaletteMode) =>
-          prevColor === "light" ? "dark" : "light"
+        setMode((prevColor: PaletteMode) => {
+          const newMode = prevColor === "light" ? "dark" : "light"
+          localStorage.setItem('mode', newMode)
+          return newMode;
+        }
         );
       },
     }),
@@ -31,7 +38,6 @@ export default function App({ Component, pageProps }: AppProps) {
 
   const theme = useMemo(() => createTheme(getDesignTokens(mode)), [mode]);
 
-  console.log(mode)
   return (
     <>
       <ColorModeContext.Provider value={colorMode}>
