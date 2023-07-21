4900f88aedea93c63f2e8baaffa69557506af6cb
NelsonDong41 <nelsondong41@gmail.com>
2023-07-21 16:28:10 -0400

    starting framer motion animations

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,15 @@ import Layout from "@/components/Layout"
 import { Button, Container } from "@mui/material"
 import { useContext } from "react"
 import { ColorModeContext } from "./_app"
+import Signature from "@/components/images/Signature"
+import {motion} from 'framer-motion'
 
 export default function HomePage() {
     const colorMode = useContext(ColorModeContext)
     return <>
         <Layout>
             <Button variant="contained" onClick={() => colorMode.toggleColorMode!()}>hello</Button>
+            <Signature/>
         </Layout>
     </>
 }
